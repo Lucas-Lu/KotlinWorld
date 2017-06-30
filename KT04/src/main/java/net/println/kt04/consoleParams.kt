4a54e5d0f7b457a41a025089e72e040a29170bd8
Lucas Lu <Lucas-Lu@users.noreply.github.com>
2017-06-30 23:32:04 +0800

    kt04

---
+++ KT04/src/main/java/net/println/kt04/consoleParams.kt
@@ -0,0 +1,8 @@
+package net.println.kt04
+
+/**
+ * Created by luliju on 2017/6/30.
+*/
+fun main(args: Array<String>) {
+    args.map(::println)
+}
---
+++ KT04/src/main/java/net/println/kt04/main.java
@@ -0,0 +1,12 @@
+package net.println.kt04;
+
+/**
+ * Created by luliju on 2017/6/30.
+ */
+public class main {
+    public static void main(String...args){
+        for(String arg : args){
+            System.out.println(arg);
+        }
+    }
+}
