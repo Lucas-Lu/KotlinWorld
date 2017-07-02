bfaf3a7b187a7735b35e2b4616a7b3f9d901f860
Lucas Lu <Lucas-Lu@users.noreply.github.com>
2017-07-02 15:34:03 +0800

    KT07

---
+++ KT07/src/main/java/net/println/kt07/Lambda.kt
@@ -0,0 +1,13 @@
+package net.println.kt07
+
+import java.io.File
+import java.util.*
+import rx.Observable
+
+/**
+ * Created by luliju on 2017/7/2.
+ */
+fun main(args:Array<String>){
+    val text = File(ClassLoader.getSystemResource("code").path).readText()
+    Observable.from()
+}
