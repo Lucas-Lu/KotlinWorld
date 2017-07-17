ef185c26efad32bca6a8ba22887b85ffbc9ae111
Lucas Lu <Lucas-Lu@users.noreply.github.com>
2017-07-17 19:31:47 +0800

    update

--- KT14/src/main/java/net/println/kt14/CallExtensionMethod.java
+++ KT14/src/main/java/net/println/kt14/CallExtensionMethod.java
@@ -4,4 +4,8 @@ package net.println.kt14;
  * Created by luliju on 2017/7/17.
  */
 public class CallExtensionMethod {
+    public static void main(String...args){
+        String s = "Hello Main";
+        System.out.print(ExtensionMethodKt.IsEmpty(s));
+    }
 }
--- KT14/src/main/kotlin/net/println/kt14/ExtensionMethod.kt
+++ KT14/src/main/kotlin/net/println/kt14/ExtensionMethod.kt
@@ -3,3 +3,6 @@ package net.println.kt14
 /**
  * Created by luliju on 2017/7/17.
  */
+fun String.IsEmpty():Boolean{
+    return this != ""
+}
