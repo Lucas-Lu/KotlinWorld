55e5410e3c98004d1c74c8fa46d806020e765f03
Lucas Lu <Lucas-Lu@users.noreply.github.com>
2017-07-19 19:33:10 +0800

    add KT15

---
+++ KT15/src/main/java/net/println/kt15/SAMInJava.java
@@ -0,0 +1,7 @@
+package net.println.kt15;
+
+/**
+ * Created by luliju on 2017/7/19.
+ */
+public class SAMInJava {
+}
---
+++ KT15/src/main/kotlin/net/println/kt15/AccessDataClass.kt
@@ -0,0 +1,5 @@
+package net.println.kt15
+
+/**
+ * Created by luliju on 2017/7/19.
+ */
