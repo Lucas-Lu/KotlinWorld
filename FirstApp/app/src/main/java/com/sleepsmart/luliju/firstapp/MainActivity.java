b2134a2cf157063420939342f2e9848f407e14f7
Lucas Lu <Lucas-Lu@users.noreply.github.com>
2017-08-04 18:32:27 +0800

    play kotlin-android

--- FirstApp/app/src/main/java/com/sleepsmart/luliju/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/sleepsmart/luliju/firstapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.sleepsmart.luliju.firstapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView testText = (TextView) findViewById(R.id.editText);
+        testText.setText(
+                "TTT"
+        );
+
+        Intent intent = new Intent();
+        intent.setClass(this, Main2Activity.class);
+        startActivity(intent);
 
     }
 }
