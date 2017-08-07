d56aaf89d96cdf5674cc5a12941395ec6ac3dd75
Lucas Lu <Lucas-Lu@users.noreply.github.com>
2017-08-07 19:41:41 +0800

    Login Page

--- FirstApp/app/src/main/java/com/sleepsmart/luliju/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/sleepsmart/luliju/firstapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.sleepsmart.luliju.firstapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +17,19 @@ public class MainActivity extends AppCompatActivity {
                 "TTT"
         );
 
+        Button Btnswitch = (Button) findViewById(R.id.Switch);
+        Btnswitch.setOnClickListener(listener);
         Intent intent = new Intent();
-        intent.setClass(this, Main2Activity.class);
+        intent.setClass(this, LoginActivity.class);
         startActivity(intent);
 
     }
+
+    Button.OnClickListener listener = new Button.OnClickListener(){//创建监听对象
+        public void onClick(){
+            String strTmp="点击Button02";
+        }
+
+    };
+
 }
---
+++ FirstApp/app/src/main/java/com/sleepsmart/luliju/firstapp/SignActivity.kt
@@ -0,0 +1,12 @@
+package com.sleepsmart.luliju.firstapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign)
+    }
+}
